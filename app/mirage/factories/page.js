c251ffa5e6b248d9bb36aa27ca8a49bb40fc1318
David Robson <david.robson@appsbroker.com>
2016-02-02 19:18:43 +0000

    ui setup with test data

---
+++ app/mirage/factories/page.js
@@ -0,0 +1,26 @@
+/*
+  This is an example factory definition.
+
+  Create more files in this directory to define additional factories.
+*/
+import Mirage/*, {faker} */ from 'ember-cli-mirage';
+
+export default Mirage.Factory.extend({
+
+  title() { return 'test'; },
+  author() { return 'David Robson'; },
+  content() { return 'Loads of awsome content'; }
+
+
+  // name: 'Pete',                         // strings
+  // age: 20,                              // numbers
+  // tall: true,                           // booleans
+
+  // email: function(i) {                  // and functions
+  //   return 'person' + i + '@test.com';
+  // },
+
+  // firstName: faker.name.firstName,       // using faker
+  // lastName: faker.name.firstName,
+  // zipCode: faker.address.zipCode
+});
---
+++ app/mirage/scenarios/default.js
@@ -0,0 +1,11 @@
+export default function(server) {
+
+  // Seed your development database using your factories. This
+  // data will not be loaded in your tests.
+
+  // server.createList('contact', 10);
+
+  var page = server.create('page');
+
+  console.log(page);
+}
