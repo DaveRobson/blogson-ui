a1c8a2ce9451bfae763e80e2056c914084d7cdbd
David Robson <david.robson@appsbroker.com>
2016-02-14 11:16:28 +0000

    Template creation using editor

--- app/routes/admin/page.js
+++ app/routes/admin/page.js
@@ -1,11 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   model() {
     return {
       mode: { name: 'handlebars', base: 'text/html'},
-      theme: 'monokai'
+      theme: 'monokai',
+      page: {title: '', author: '', name: ''}
     };
+  },
+  actions: {
+    create(model) {
+
+      const template = Ember.$('.template')[0].value;
+      const page = model.page;
+
+      this.store.createRecord('page', {
+        id: page.name,
+        title: page.name,
+        author: page.author,
+        template: template,
+        payload: {
+          editor: {
+            content: '<p>Dave Test content</p>',
+            header: 'Whatever'
+          }
+        }
+      }).save();
+
+    }
   }
+
 
 });
